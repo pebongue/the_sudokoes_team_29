14acd1262a05f8642daf8ffd9c1f79f1c0e12d0a
MJtsholo <tsholofelo.mautjana@umuzi.org>
2021-03-12 22:16:33 +0200

    Added file for opening puzzle

---
+++ board_opener.py
@@ -0,0 +1,17 @@
+myfile = open("sample.txt")
+
+def print_soduko_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -  ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+print_soduko_board(myfile)
