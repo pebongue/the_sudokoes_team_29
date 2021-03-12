4693a33ff16e62e006cd39da744ec4eefdb56b83
MJtsholo <tsholofelo.mautjana@umuzi.org>
2021-03-12 23:50:41 +0200

    updated board opener

--- board_opener.py
+++ board_opener.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def open_file(filename):
-    file = open(filename)
-    print(np.genfromtxt(file))
+    file = open(filename, "r")
+    list_file = file.read().split(",\n ")
+    array_file = np.array(list_file)
+    print(array_file)
+    print(type(array_file))
     
 open_file("sample.txt")
