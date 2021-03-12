6491915c14611f4ae80692f746ea5b32e8c765a0
MJtsholo <tsholofelo.mautjana@umuzi.org>
2021-03-13 00:40:53 +0200

    updated board opener

--- board_opener.py
+++ board_opener.py
@@ -2,9 +2,10 @@ import numpy as np
 
 def open_file(filename):
     file = open(filename, "r")
-    list_file = file.read().split(",\n ")
-    array_file = np.array(list_file)
-    print(array_file)
-    print(type(array_file))
+    list_file = file.read().split("\n")
+    structure = '\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in list_file])
+    arrayS = np.array(structure)
+    print(arrayS)
+    print(type(arrayS))
     
 open_file("sample.txt")
