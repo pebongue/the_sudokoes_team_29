7dd49c4f433b9f7e124cb84dcc8425ad38319024
pebongue <placide.ebongue@gmail.com>
2021-03-12 23:56:12 +0200

    no comment

--- board_opener.py
+++ board_opener.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def open_file(filename):
-    file = open(filename)
-    print(np.genfromtxt(file))
+    file = open(filename, "rb").readlines()
+    print(np.load(file, allow_pickle=True))
     
 open_file("sample.txt")
