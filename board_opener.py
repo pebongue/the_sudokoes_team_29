030102fc10e1bee7e420167fbaf65036c5e50a32
MJtsholo <tsholofelo.mautjana@umuzi.org>
2021-03-12 22:41:48 +0200

    updated board opener

--- board_opener.py
+++ board_opener.py
@@ -1,27 +1,7 @@
-myfile = open("sample.txt")
-
-def print_soduko_board(board):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - -  ")
-
-        for j in range(len(board[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
-            if j == 8:
-                print(board[i][j])
-            else:
-                print(str(board[i][j]) + " ", end="")
-
-print_soduko_board(myfile)
-
-
-'''import numpy as np
+import numpy as np
 
 def open_file(filename):
     file = open(filename)
-    return np.array(file)
+    print(np.genfromtxt(file))
     
-print(open_file("sample.txt"))
-'''
+open_file("sample.txt")
