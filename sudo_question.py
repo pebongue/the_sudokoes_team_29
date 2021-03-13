49139050bc4a171dfd25d0bb185dd6a33837904d
pebongue <placide.ebongue@gmail.com>
2021-03-13 10:39:48 +0200

    I added comments field for the tasks

--- sudo_question.py
+++ sudo_question.py
@@ -1,6 +1,26 @@
 # You may create additional functions here:
+'''Level 1 - solutions will go here
+
+    1. read from a text file, load into an np array and return it
+    2. print a sudoku grid from the array read from the file
+    3. allow a user to substitute value into the grid
+'''
 
 
+'''Level 2 - solutions will go here
+
+    1. after the grid or puzzle has been filled with candidate values, display whether or not the user solution is correct
+    2. Ask the user if he wants to play again or just select a puzzle for the system to solve
+'''
+
+'''Level 3 - solutions will go here
+
+    1. This is the same as Level 2 - 2
+    2. When user select solve puzzle, type in a file name, read files from a folder of unsolved puzzle and display
+        a. if the user selected in Level 2 - 2 to continue playing, load grid and allow user to input values, then check if puzzle is solved
+        b. if the user selected just to solve puzzle - do Level 3 - 3 
+    3. the program must automatically solve the puzzle
+'''
 
 # Additional Functions above this comment
 
